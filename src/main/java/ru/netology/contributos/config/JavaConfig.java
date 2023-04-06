85ab848c2dbeccb2dbf260298f6be58e5e41bb67
Евгений <avikon3412@yandex.ru>
2023-04-06 12:55:25 +0300

    исправил условия вызова BEAN

--- src/main/java/ru/netology/contributos/config/JavaConfig.java
+++ src/main/java/ru/netology/contributos/config/JavaConfig.java
@@ -10,13 +10,13 @@ import ru.netology.contributos.profile.SystemProfile;
 @Configuration
 public class JavaConfig {
     @Bean
-    @ConditionalOnProperty(name = "netology.profile.dev", havingValue = "true")
+    @ConditionalOnProperty(value = "netology.profile.dev", havingValue = "true", matchIfMissing = true)
     public SystemProfile devProfile() {
         return new DevProfile();
     }
 
     @Bean
-    @ConditionalOnProperty(name = "netology.profile.dev", havingValue = "false")
+    @ConditionalOnProperty(value = "netology.profile.dev", havingValue = "false")
     public SystemProfile prodProfile() {
         return new ProductionProfile();
     }
